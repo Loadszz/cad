2beca3a43b395ab8ccc82866addc25a1bb605be6
Loadszz <loasdzz94@gmail.com>
2023-11-14 20:44:59 +0200

    new1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
 					</div>
 					<button
 						type="button"
-						className={`hidden backdrop-blur-[21px] bg-myBrown rounded-[32px] border border-[#B0B0B0] w-[44px] h-[44px] lp:block z-10 transition-all duration-700 ${isOpen ? 'border-none' : ''}`}
+						className={`hidden backdrop-blur-[21px] bg-myBrown rounded-[32px] border border-[#B0B0B0] w-[44px] h-[44px] lp:block z-10 transition-all duration-700 relative ${isOpen ? 'border-none' : ''}`}
 						onClick={handleBurger}>
 						<span className={`absolute w-[20px] h-[3px] top-[12px] bg-white rounded-[32px] translate-x-[-50%] left-[50%] transition-all duration-300 ${isOpen ? 'rotate-[45deg] top-[50%]' : ''}`}></span>
 						<span className={`absolute top-1/2 transform -translate-y-1/2 w-[20px] h-[3px] bg-white rounded-[32px] translate-x-[-50%] left-[50%] ${isOpen ? 'hidden' : ''}`}></span>
