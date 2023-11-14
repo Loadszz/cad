794719ce270bd6e2ddc4ed7b9e422b73e5135361
Loadszz <loasdzz94@gmail.com>
2023-11-14 20:16:07 +0200

    new1

--- src/components/Advantages.jsx
+++ src/components/Advantages.jsx
@@ -36,7 +36,7 @@ const Advantages = () => {
 							</div>
 						</div>
 						{/* item-left */}
-						<div className='w-full flex flex-col items-start'>
+						<div className='w-full flex flex-col items-start mb-[120px] tb:mb-[40px]'>
 							<div className='flex items-center tb:flex-row-reverse w-[50%] tb:w-full mb-[32px]'>
 								{/* circle */}
 								<div className='advantage-circle-number mr-[30px] tb:ml-[30px] tb:mr-0'>02</div>
@@ -50,6 +50,96 @@ const Advantages = () => {
 								<div className='flex-[1_1_auto] mr-[40px] tb:mr-0'>
 									<h3 className='advantage-item-title max-w-[464px] mb-[24px] tb:mb-[12px]'>{t('advantages.item-title-second')}</h3>
 									<div className='advantage-item-text'>{t('advantages.item-text-second')}</div>
+								</div>
+							</div>
+						</div>
+						{/* item-right */}
+						<div className='w-full flex flex-col items-end mb-[120px] tb:mb-[40px] group'>
+							<div className='flex items-center w-[50%] tb:w-full mb-[32px] group'>
+								{/* circle */}
+								<div className="advantage-circle-icon relative left-[-25px] lp:left-[-20px] tb:left-[-11px] z-10 group-hover:bg-cyan-400 transition-all duration-300">
+									<div className='absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] rounded-[350px] h-[20px] tb:h-[15px] w-[20px] tb:w-[15px] bg-white'></div>
+								</div>
+								<div className='border border-dashed border-[#919191] w-[100%]'></div>
+								<div className='advantage-circle-number ml-[30px]'>01</div>
+							</div>
+							<div className='flex justify-end items-start w-[50%] tb:w-full'>
+								<div className='advantage-item-icon icon-advantages-globe flex items-center justify-center flex-[0_0_130px] lp:flex-[0_0_100px] tb:flex-[0_0_60px] mr-[40px] lp:mr-[24px] tb:mr-[12px] ml-[40px]'></div>
+								<div className='flex-[1_1_auto]'>
+									<h3 className='advantage-item-title max-w-[464px] mb-[24px] tb:mb-[12px]'>{t('advantages.item-title-first')}</h3>
+									<div className='advantage-item-text'>{t('advantages.item-text-first')}</div>
+								</div>
+							</div>
+						</div>
+						{/* item-left */}
+						<div className='w-full flex flex-col items-start mb-[120px] tb:mb-[40px]'>
+							<div className='flex items-center tb:flex-row-reverse w-[50%] tb:w-full mb-[32px]'>
+								{/* circle */}
+								<div className='advantage-circle-number mr-[30px] tb:ml-[30px] tb:mr-0'>02</div>
+								<div className='border border-dashed border-[#919191] w-[100%]'></div>
+								<div className="advantage-circle-icon relative right-[-25px] lp:right-[-20px] tb:left-[-11px] z-10">
+									<div className='absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] rounded-[350px] h-[20px] tb:h-[15px] w-[20px] tb:w-[15px] bg-white'></div>
+								</div>
+							</div>
+							<div className='flex justify-end items-start w-[50%] tb:w-full'>
+								<div className='advantage-item-icon icon-advantages-person flex items-center justify-center flex-[0_0_130px] lp:flex-[0_0_100px] tb:flex-[0_0_60px] mr-[40px] lp:mr-[24px] tb:mr-[12px] tb:ml-[40px]'></div>
+								<div className='flex-[1_1_auto] mr-[40px] tb:mr-0'>
+									<h3 className='advantage-item-title max-w-[464px] mb-[24px] tb:mb-[12px]'>{t('advantages.item-title-second')}</h3>
+									<div className='advantage-item-text'>{t('advantages.item-text-second')}</div>
+								</div>
+							</div>
+						</div>
+						{/* item-right */}
+						<div className='w-full flex flex-col items-end mb-[120px] tb:mb-[40px] group'>
+							<div className='flex items-center w-[50%] tb:w-full mb-[32px] group'>
+								{/* circle */}
+								<div className="advantage-circle-icon relative left-[-25px] lp:left-[-20px] tb:left-[-11px] z-10 group-hover:bg-cyan-400 transition-all duration-300">
+									<div className='absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] rounded-[350px] h-[20px] tb:h-[15px] w-[20px] tb:w-[15px] bg-white'></div>
+								</div>
+								<div className='border border-dashed border-[#919191] w-[100%]'></div>
+								<div className='advantage-circle-number ml-[30px]'>01</div>
+							</div>
+							<div className='flex justify-end items-start w-[50%] tb:w-full'>
+								<div className='advantage-item-icon icon-advantages-globe flex items-center justify-center flex-[0_0_130px] lp:flex-[0_0_100px] tb:flex-[0_0_60px] mr-[40px] lp:mr-[24px] tb:mr-[12px] ml-[40px]'></div>
+								<div className='flex-[1_1_auto]'>
+									<h3 className='advantage-item-title max-w-[464px] mb-[24px] tb:mb-[12px]'>{t('advantages.item-title-first')}</h3>
+									<div className='advantage-item-text'>{t('advantages.item-text-first')}</div>
+								</div>
+							</div>
+						</div>
+						{/* item-left */}
+						<div className='w-full flex flex-col items-start mb-[120px] tb:mb-[40px]'>
+							<div className='flex items-center tb:flex-row-reverse w-[50%] tb:w-full mb-[32px]'>
+								{/* circle */}
+								<div className='advantage-circle-number mr-[30px] tb:ml-[30px] tb:mr-0'>02</div>
+								<div className='border border-dashed border-[#919191] w-[100%]'></div>
+								<div className="advantage-circle-icon relative right-[-25px] lp:right-[-20px] tb:left-[-11px] z-10">
+									<div className='absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] rounded-[350px] h-[20px] tb:h-[15px] w-[20px] tb:w-[15px] bg-white'></div>
+								</div>
+							</div>
+							<div className='flex justify-end items-start w-[50%] tb:w-full'>
+								<div className='advantage-item-icon icon-advantages-person flex items-center justify-center flex-[0_0_130px] lp:flex-[0_0_100px] tb:flex-[0_0_60px] mr-[40px] lp:mr-[24px] tb:mr-[12px] tb:ml-[40px]'></div>
+								<div className='flex-[1_1_auto] mr-[40px] tb:mr-0'>
+									<h3 className='advantage-item-title max-w-[464px] mb-[24px] tb:mb-[12px]'>{t('advantages.item-title-second')}</h3>
+									<div className='advantage-item-text'>{t('advantages.item-text-second')}</div>
+								</div>
+							</div>
+						</div>
+						{/* item-right */}
+						<div className='w-full flex flex-col items-end mb-[120px] tb:mb-[40px] group'>
+							<div className='flex items-center w-[50%] tb:w-full mb-[32px] group'>
+								{/* circle */}
+								<div className="advantage-circle-icon relative left-[-25px] lp:left-[-20px] tb:left-[-11px] z-10 group-hover:bg-cyan-400 transition-all duration-300">
+									<div className='absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] rounded-[350px] h-[20px] tb:h-[15px] w-[20px] tb:w-[15px] bg-white'></div>
+								</div>
+								<div className='border border-dashed border-[#919191] w-[100%]'></div>
+								<div className='advantage-circle-number ml-[30px]'>01</div>
+							</div>
+							<div className='flex justify-end items-start w-[50%] tb:w-full'>
+								<div className='advantage-item-icon icon-advantages-globe flex items-center justify-center flex-[0_0_130px] lp:flex-[0_0_100px] tb:flex-[0_0_60px] mr-[40px] lp:mr-[24px] tb:mr-[12px] ml-[40px]'></div>
+								<div className='flex-[1_1_auto]'>
+									<h3 className='advantage-item-title max-w-[464px] mb-[24px] tb:mb-[12px]'>{t('advantages.item-title-first')}</h3>
+									<div className='advantage-item-text'>{t('advantages.item-text-first')}</div>
 								</div>
 							</div>
 						</div>
